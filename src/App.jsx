9208ea86ec39dd0620664998b9f41964dc4dfe45
shouravrahman <shouravrahman006@gmail.com>
2023-12-01 03:23:51 +0600

    feat: faq,contact,footer,config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Contact from "./components/Contact";
+import Faq from "./components/Faq";
+import Footer from "./components/Footer";
+import Testimonials from "./components/Testimonials";
+
 function App() {
-	return <div>hello</div>;
+	return (
+		<div className='text-base bg-background-dark p-12'>
+			<Testimonials />
+			<Faq />
+			<Contact />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: [
+		"./src/**/*.{js,ts,jsx,tsx,mdx}",
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				"background-dark": "#111928",
+				"text-dark": "#f5f5f5",
+				"background-light": "#f5f5f5",
+				"text-light": "#111928",
+				"faq-background-dark": "#111827",
+				"icon-fill-dark": "#93c5fd",
+				"testimonial-citation": "#9ca3af",
+				"testimonial-bg-dark": "#1F2937",
+			},
+		},
+	},
+	plugins: [],
+};
