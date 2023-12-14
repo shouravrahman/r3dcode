fbedcda37a2f164fe0af628dd939aaf2f701de24
shouravrahman <shouravrahman006@gmail.com>
2023-12-14 19:26:43 +0600

    lint: fix eslint errors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,9 @@
-import { LayoutGrid } from "lucide-react";
-import { LayoutGridIcon } from "lucide-react";
-import { LogIn } from "lucide-react";
-import { Search } from "lucide-react";
-
 const Navbar = () => {
-	const links = [
-		{ title: "Service", href: "/", img: "/service-white.svg" },
-		{ title: "Pricing", href: "/", img: "/price-white.svg" },
-		{ title: "Contact", href: "/", img: "/contact-white.svg" },
-	];
+	// const links = [
+	// 	{ title: "Service", href: "/", img: "/service-white.svg" },
+	// 	{ title: "Pricing", href: "/", img: "/price-white.svg" },
+	// 	{ title: "Contact", href: "/", img: "/contact-white.svg" },
+	// ];
 	return (
 		<header>
 			<nav>
@@ -63,15 +58,6 @@ const Navbar = () => {
 									<button className='text-white bg-blue-600 hover:bg-blue-700 ml-4 px-3 py-2 rounded-md text-lg font-medium'>
 										Login
 									</button>
-									{/* <div className='ml-3 relative'>
-										<div className='relative inline-block text-left'>
-											<div>
-												<button className='bg-blue-800 p-1 rounded-full text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-blue-800 focus:ring-white'>
-													<LayoutGridIcon className='h-6 w-6' />
-												</button>
-											</div>
-										</div>
-									</div> */}
 								</div>
 							</div>
 						</div>
@@ -79,71 +65,6 @@ const Navbar = () => {
 					<div className='border-t border-gray-700' />
 				</div>
 			</nav>
-
-			{/* <nav id='secondNav' className='text-blue-900 lg:hidden'>
-				<div className='fixed bottom-0 left-0 right-0 p-2 shadow bg-slate-50'>
-					<ul className='flex items-center justify-between'>
-						<li>
-							<Link href='#'>
-								<div className='w-6 h-6 mx-auto text-center text-text-dark bg-blue-700 rounded-full'>
-									DB
-								</div>
-								<div className='text-lg'>DBook</div>
-							</Link>
-						</li>
-						<li>
-							<Link href='#'>
-								<img
-									className='w-5 h-5 mx-auto'
-									src='/static_files/svgs5/book.svg'
-									alt='book'
-								/>
-								<div className='text-lg'>Book</div>
-							</Link>
-						</li>
-						<li>
-							<Link href='#'>
-								<img
-									className='w-5 h-5 mx-auto'
-									src='/static_files/svgs5/dpad.svg'
-									alt='Navigate'
-								/>
-								<div className='text-lg'>Navigate</div>
-							</Link>
-						</li>
-						<li>
-							<a href='#'>
-								<img
-									className='w-5 h-5 mx-auto'
-									src='/static_files/svgs5/bar-chart.svg'
-									alt='bar-chart'
-								/>
-								<div className='text-lg'>Stats</div>
-							</Link>
-						</li>
-						<li>
-							<a href='#'>
-								<img
-									className='w-5 h-5 mx-auto'
-									src='/static_files/svgs5/lightbulb.svg'
-									alt='lightbulb'
-								/>
-								<div className='text-lg'>Idea</div>
-							</Link>
-						</li>
-						<li>
-							<a href='#'>
-								<img
-									className='w-5 h-5 mx-auto'
-									src='/static_files/svgs5/briefcase.svg'
-									alt='briefcase'
-								/>
-								<div className='text-lg'>Career</div>
-							</Link>
-						</li>
-					</ul>
-				</div>
-			</nav> */}
 		</header>
 	);
 };
