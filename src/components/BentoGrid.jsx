32ffe70b11ed7e21ce9f5437be92c04a42fa3d75
shouravrahman <shouravrahman006@gmail.com>
2023-12-14 19:24:51 +0600

    fix: responsiveness

--- src/components/BentoGrid.jsx
+++ src/components/BentoGrid.jsx
@@ -1,15 +1,18 @@
 const BentoGrid = () => {
 	return (
-		<div className='grid mx-auto py-12 px-4 w-[70vw] auto-rows-[192px] grid-cols-3 gap-4'>
-			{[...Array(4)].map((_, i) => (
-				<div
-					key={i}
-					className={`row-span-2 rounded-xl shadow-lg bg-neutral-100 p-4 dark:bg-[#1D273E] ${
-						i === 0 && "col-span-3"
-					}              
+		<div className='grid mx-auto py-12 px-4 w-[70vw] auto-rows-[300px] grid-cols-3 gap-4'>
+			<div
+				className={`col-span-3 rounded-xl shadow-lg bg-neutral-100 p-4 dark:bg-[#1D273E]             
                `}
-				></div>
-			))}
+			></div>
+			<div
+				className={`col-span-3 md:col-span-1 rounded-xl shadow-lg bg-neutral-100 p-4 dark:bg-[#1D273E]             
+               `}
+			></div>
+			<div
+				className={`col-span-3 md:col-span-2 rounded-xl shadow-lg bg-neutral-100 p-4 dark:bg-[#1D273E]             
+               `}
+			></div>
 		</div>
 	);
 };
